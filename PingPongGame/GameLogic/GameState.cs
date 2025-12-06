adc29b1352b28419fc2037c21ec0a0335b3ac756
Mostal4ik <kosta549103@mail.ru>
2025-12-06 21:47:27 +0300

    Добавлена логика для GameState.cs

--- PingPongGame/GameLogic/GameState.cs
+++ PingPongGame/GameLogic/GameState.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PingPongGame.GameLogic
+﻿namespace PingPongGame.GameLogic
 {
-    internal class GameState
+    public enum GameState
     {
+        WaitingToStart, // ждём начала розыгрыша (мяч стоит)
+        Playing,        // идёт игра
+        Paused,         // пауза
+        GameOver        // игра закончена (кто-то набрал MaxScore)
     }
 }
