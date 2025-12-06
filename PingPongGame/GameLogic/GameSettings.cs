a11a234de554a69b214b0f4091f397af3acfce3c
Mostal4ik <kosta549103@mail.ru>
2025-12-06 21:52:08 +0300

    Добавлена логика GameSettings.cs

--- PingPongGame/GameLogic/GameSettings.cs
+++ PingPongGame/GameLogic/GameSettings.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PingPongGame.GameLogic
+﻿namespace PingPongGame.GameLogic
 {
-    internal class GameSettings
+    public class GameSettings
     {
+        public int FieldWidth { get; }
+        public int FieldHeight { get; }
+        public int MaxScore { get; }
+
+        public float BallRadius { get; } = 8f;
+        public float BallSpeed { get; } = 250f;      // пикселей в секунду
+        public float PaddleWidth { get; } = 10f;
+        public float PaddleHeight { get; } = 80f;
+        public float PaddleSpeed { get; } = 300f;    // пикселей в секунду
+
+        public GameSettings(int fieldWidth, int fieldHeight, int maxScore = 5)
+        {
+            FieldWidth = fieldWidth;
+            FieldHeight = fieldHeight;
+            MaxScore = maxScore;
+        }
     }
 }
