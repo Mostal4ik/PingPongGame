7984017a5f05359c675751f34cba806be2df0d40
Mostal4ik <kosta549103@mail.ru>
2025-12-06 22:01:44 +0300

    Обновлён GameSettings.cs (с учётом сложности)

--- PingPongGame/GameLogic/GameSettings.cs
+++ PingPongGame/GameLogic/GameSettings.cs
@@ -10,13 +10,20 @@
         public float BallSpeed { get; } = 250f;      // пикселей в секунду
         public float PaddleWidth { get; } = 10f;
         public float PaddleHeight { get; } = 80f;
-        public float PaddleSpeed { get; } = 300f;    // пикселей в секунду
+        public float PaddleSpeed { get; } = 300f;    // базовая скорость ракетки
 
-        public GameSettings(int fieldWidth, int fieldHeight, int maxScore = 5)
+        public AIDifficulty AIDifficulty { get; }
+
+        public GameSettings(
+            int fieldWidth,
+            int fieldHeight,
+            int maxScore = 5,
+            AIDifficulty aiDifficulty = AIDifficulty.Normal)
         {
             FieldWidth = fieldWidth;
             FieldHeight = fieldHeight;
             MaxScore = maxScore;
+            AIDifficulty = aiDifficulty;
         }
     }
 }
